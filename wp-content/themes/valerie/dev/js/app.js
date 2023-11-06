bdbfccbc39f3a01b04132a8f69f2e98f13601cc1
Hugo Chen <hugochen@Hugos-MBP-2.lan>
2023-11-06 11:11:53 -0800

    updated Themes name.

--- wp-content/themes/valerie/dev/js/app.js
+++ wp-content/themes/valerie/dev/js/app.js
@@ -1,23 +1,5 @@
-import  FilterResources from './modules/training-edu-resources';
 import 'waypoints/lib/noframework.waypoints.min.js';
 import './modules/smooth-scroll';
 import './modules/nav';
 import './modules/form';
 import './modules/flickity';
-import './modules/gallery-lightbox';
-import './modules/gallery-lightbox-new';
-import './modules/working-groups';
-import './modules/about-scroll-nav';
-import './modules/core-facilities';
-import './modules/jobs';
-import './modules/events';
-import './modules/event-toggle';
-import './modules/cookie-banner';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const filterResources = document.querySelector('[filter-resources]')
-
-  if (filterResources) {
-    new FilterResources(filterResources)
-  }
-})
