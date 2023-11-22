0cf37636b559497068d01dc8c63184e9ceff84e7
Hugo Chen <hugochen@Hugos-MBP-2.lan>
2023-11-21 20:02:28 -0800

    hook up google form.

--- wp-content/themes/lantern-foundation/templates/home.php
+++ wp-content/themes/lantern-foundation/templates/home.php
@@ -6,6 +6,32 @@
 <h1>Home page</h1>
 
 
+
+
+  <section class="form--sec">
+    <div class="container">
+      <h2>Send Us Message</h2>
+      <form>
+        <div class="row">
+          <div class="half">
+            <input type="text" class="input-required" id="fname" name="fname" placeholder="First Name">
+          </div>
+          <div class="half">
+            <input type="text" id="lname" name="lname" placeholder="Last Name">
+          </div>
+        </div>
+        <div class="row">
+          <input type="email" name="email" placeholder="Email">
+        </div>
+        <div class="row">
+          <input type="email" name="email" placeholder="Email">
+        </div>
+        <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Leave Your Message">
+
+        </textarea>
+      </form>
+    </div>
+  </section>
 <?php
 get_footer();
 ?>
--- wp-content/themes/lantern-foundation/templates/index.php
+++ wp-content/themes/lantern-foundation/templates/index.php
@@ -5,7 +5,27 @@
 ?>
 <h1>Home page</h1>
 
+<section class="form--sec">
+  <div class="container">
+    <h2>Send Us Message</h2>
+    <form class="contact-us--form">
+      <div class="row">
+        <div class="half">
+          <input type="text" class="input-required" id="fname" name="entry.1865447532" placeholder="First Name">
+        </div>
+        <div class="half">
+          <input type="text" id="lname" name="entry.646231686" placeholder="Last Name">
+        </div>
+      </div>
+      <div class="row">
+        <input type="email" name="entry.542059978" placeholder="Email">
+      </div>
 
+      <textarea id="w3review" name="entry.62305795" rows="4" cols="50" placeholder="Leave Your Message"></textarea>
+      <input id="contact-us-submit" type="submit" value="Submit">
+    </form>
+  </div>
+</section>
 <?php
 get_footer();
 ?>
