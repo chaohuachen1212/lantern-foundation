5c66de0313761eece4a86cae5bf12ea83a1f0225
王兰莎 <chenchen@ous-MacBook-Pro.Starry-inc.net>
2023-12-19 01:02:01 -0800

    update ty page

---
+++ wp-content/themes/lantern-foundation/templates/thank-you.php
@@ -0,0 +1,23 @@
+<?php
+  // Template Name: Thank You
+  update_option('body_class', 'page-thank you theme-magenta');
+  get_header();
+?>
+
+<section class="thank-you">
+  <img src="<?php echo GET_URI ?>/img/Group 1.png">
+  <div class="container">
+      <div class"box">
+        <h1>THANK YOU</h1>
+        <p>Thanks for your information, we will be in touch.</p>
+        <a class="btn" href="/">
+          HOME PAGE
+        </a>
+      </div>
+  </div>
+</section>
+
+
+<?php
+get_footer();
+?>
