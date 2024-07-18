62198b9312d2f8dd962def4bcc5f1d8ef9007a22
Hugo Chen <hugochen@Hugos-MBP-2.lan>
2024-07-17 18:15:29 -0700

    header & news updates.

---
+++ wp-content/themes/lantern-foundation/templates/news-updates.php
@@ -0,0 +1,20 @@
+<?php
+  // Template Name: News & Update
+  update_option('body_class', 'news-updates theme-magenta new-nav');
+  get_header();
+?>
+
+  <section class="news-updates--main">
+    <div class="container">
+      <h1>News & Updates</h1>
+      <p>Welcome to our newly-added News & Updates page! We hope to provide you with continuous updates of what we have been working on—our latest projects, recent events, new collaborations, and more. Read below to learn more!</p>
+
+      <div class="rows-wrap">
+
+      </div>
+    </div>
+  </section>
+
+<?php
+get_footer();
+?>
