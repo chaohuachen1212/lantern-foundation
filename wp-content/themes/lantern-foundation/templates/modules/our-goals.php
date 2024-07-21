61178203f05e73ce8450376417c724c4bc2cc78d
Hugo Chen <hugochen@Hugos-MBP-2.lan>
2024-07-20 19:54:21 -0700

    build intro for about page.

--- wp-content/themes/lantern-foundation/templates/modules/our-goals.php
+++ wp-content/themes/lantern-foundation/templates/modules/our-goals.php
@@ -1,5 +1,23 @@
-<section class="about--intro">
+<section class="about--goals">
   <div class="container">
     <h2>Our Goals</h2>
+
+    <div class="cards-wrap">
+      <div class="card">
+        <img src="<?php echo GET_URI ?>/img/about/icon1.png">
+        <h3>Learn</h3>
+        <p>Understand the underlying causes, the main challenges, and potential solutions of rural disparities and poverty. Educate ourselves through speaking with volunteers and experts, reading about government policies, and researching about the underserved kids in rural areas. Only when we understand the problems that kids face can we find effective solutions.</p>
+      </div>
+      <div class="card">
+        <img src="<?php echo GET_URI ?>/img/about/icon1.png">
+        <h3>Learn</h3>
+        <p>Understand the underlying causes, the main challenges, and potential solutions of rural disparities and poverty. Educate ourselves through speaking with volunteers and experts, reading about government policies, and researching about the underserved kids in rural areas. Only when we understand the problems that kids face can we find effective solutions.</p>
+      </div>
+      <div class="card">
+        <img src="<?php echo GET_URI ?>/img/about/icon1.png">
+        <h3>Learn</h3>
+        <p>Understand the underlying causes, the main challenges, and potential solutions of rural disparities and poverty. Educate ourselves through speaking with volunteers and experts, reading about government policies, and researching about the underserved kids in rural areas. Only when we understand the problems that kids face can we find effective solutions.</p>
+      </div>
+    </div>
   </div>
 </section>
