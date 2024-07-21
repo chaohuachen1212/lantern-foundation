39ed6279f1e3de3a73a50b181aad1ecba74de5b5
Hugo Chen <hugochen@Hugos-MBP-2.lan>
2024-07-20 20:51:25 -0700

    out impact, card, our goals blocks.

--- wp-content/themes/lantern-foundation/templates/modules/our-impact.php
+++ wp-content/themes/lantern-foundation/templates/modules/our-impact.php
@@ -1,5 +1,48 @@
 <section class="our--impact">
   <div class="container">
     <h2>Our Impact</h2>
+
+    <div class="cards-wrap">
+      <div class="card">
+        <figure>
+          <img src="<?php echo GET_URI ?>/img/about/impact1.png">
+        </figure>
+        <article>
+          <h3>1,000+</h3>
+          <h4>people reached through our socials and events</h4>
+          <p>Website, Youtube, Instagram, WeChat, Community Events</p>
+        </article>
+      </div>
+      <div class="card">
+        <figure>
+          <img src="<?php echo GET_URI ?>/img/about/impact2.png">
+        </figure>
+        <article>
+          <h3>50,000+</h3>
+          <h4>received through donations</h4>
+          <p>These donations have gone toward covering medical treatment costs, purchasing school supplies, providing nutritional needs, paying for doctors’ travel expenses, supporting rural schools, and more</p>
+        </article>
+      </div>
+      <div class="card">
+        <figure>
+          <img src="<?php echo GET_URI ?>/img/about/impact3.png">
+        </figure>
+        <article>
+          <h3>153+</h3>
+          <h4>kids in rural China impacted across 6 different provinces</h4>
+          <p>Anhui, Henan, Gansu, Shanxi, Guangxi, Tibet</p>
+        </article>
+      </div>
+      <div class="card">
+        <figure>
+          <img src="<?php echo GET_URI ?>/img/about/impact4.png">
+        </figure>
+        <article>
+          <h3>800+</h3>
+          <h4>school supplies, personal care products, gifts and toys have been donated</h4>
+          <p>Backpacks, pencil cases, pens and pencils, toothbrushes, shampoo and lotion, puzzles, Lego boxes, bracelets, water bottles, clothes, and books.</p>
+        </article>
+      </div>
+    </div>
   </div>
 </section>
