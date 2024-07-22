05a8f40900b4489db409c4f05500a57a3e53db47
王兰莎 <chenchen@ous-MacBook-Pro.hsd1.ca.comcast.net>
2024-07-22 01:13:44 -0700

    fix heights

--- wp-content/themes/lantern-foundation/templates/donate.php
+++ wp-content/themes/lantern-foundation/templates/donate.php
@@ -4,18 +4,38 @@
   get_header();
 ?>
 
-<section class="donate-main">
+
+<section class="donate--sec" id="donate">
   <div class="container">
-
-
-    <div class="hero">
-      <h1>Donate</h1>
+    <div class="inner-wrap">
+      <h2>Donate</h2>
+      <img src="<?php echo GET_URI ?>/img/donate-img.jpg">
+      <h3>Through your donation, we can make a difference in the lives of children around the world. Donations of any amount are greatly appreciated and are guaranteed to go towards one of our campaigns and projects. Thank you for your continued support!</h3>
+      <div class="btn-wrap">
+        <a class="btn" target="_blank" href="https://square.link/u/hOHmgOj0">Donate to Lantern Foundation</a>
+      </div>
+      <div class="row">
+        <div class="col">
+          <h3>Other Ways to Donate</h3>
+          <div class="box">
+            <p>Mail Your Donation:</p>
+            <p>3962 Summit Road, Dublin CA 94568</p>
+          </div>
+        </div>
+        <div class="col">
+          <h3>Questions about Donation</h3>
+          <div class="box">
+            <p>Contact us at:</p>
+            <p><a href="mailto:lanternfoundation2023@gmail.com">lanternfoundation2023@gmail.com</a></p>
+          </div>
+        </div>
+      </div>
     </div>
-
-    <p>Through your donation, we can make a difference in the lives of children around the world. Donations of any amount are greatly appreciated and is guaranteed to go towards one of our campaigns and projects.</p>
-
   </div>
 </section>
+
+
+
 
 
 
